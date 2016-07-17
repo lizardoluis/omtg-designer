85c986a2974a8e4c19fb883168197d851dfab4cd
Luis Eduardo Oliveira Lizardo <lizardo.luis@gmail.com>
2016-07-17 19:09:44 -0300

    Removed www/dist folder. Minification is now done inside js and css folders.

--- www/js/views/navbar-view.js
+++ www/js/views/navbar-view.js
@@ -10,6 +10,7 @@
 			'click #btnImportXML' : 'importXML',
 			'click #btnExportXML' : 'exportXML',
 			'click #btnExportSQL' : 'exportSQL',
+			'click #btnExportPostgis' : 'exportPostgis',
 			'click #btnPrint' : 'print',
 			'click #btnAbout' : 'showAbout',
 		},
@@ -54,6 +55,41 @@
 
 				$.ajax({
 					url : "omtg2sql",
+					type: "POST",
+					data : xml,
+					contentType : "application/json; charset=UTF-8",
+					success: function(data, textStatus, jqXHR)
+					{
+						var byteArray = new Uint8Array(data.length);
+						for (var i = 0; i < data.length; i++) {
+							byteArray[i] = data.charCodeAt(i) & 0xff;
+						}
+						var blob = new Blob([byteArray]);
+
+						saveAs(blob, "OMTG.zip");
+					},
+					error: function(ajaxrequest, ajaxOptions, thrownError)
+					{
+						alert("The request failed.");
+					}
+				});				
+
+			}, false);
+		},	
+		
+		exportPostgis : function() {
+			
+			if(app.canvas.get('diagrams').length == 0){
+				alert(app.msgs.EMPTY_PROJECT);
+				return;
+			}
+			
+			app.plumb.doWhileSuspended(function(){				
+
+				var xml = app.canvas.toXML();
+
+				$.ajax({
+					url : "omtg2postgis",
 					type: "POST",
 					data : xml,
 					contentType : "application/json; charset=UTF-8",
