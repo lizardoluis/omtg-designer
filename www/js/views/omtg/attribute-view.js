c961df8f3fc6f68354180080328d7e337809cfd4
Luis Eduardo Oliveira Lizardo <lizardo.luis@gmail.com>
2016-11-10 11:22:30 -0200

    Change variables visualization on diagram.

--- www/js/views/omtg/attribute-view.js
+++ www/js/views/omtg/attribute-view.js
@@ -17,7 +17,7 @@
 			// TODO: include other attr fields
 			this.$el.html(this.template({
 				isKey : this.model.get('isKey'), 
-				attr : this.model.escape('name') + ': ' + this.model.get('type'),
+				attr : this.model.get('type') == 'VARCHAR' ? this.model.escape('name') + ': ' + this.model.get('type') + '('+ this.model.get('length') +')' : this.model.escape('name') + ': ' + this.model.get('type'),
 			}));
 			return this;
 		},
