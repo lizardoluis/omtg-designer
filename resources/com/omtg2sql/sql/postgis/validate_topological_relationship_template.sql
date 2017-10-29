00dd894941b51bbd3ca6d4aa2b519ebbe48bb231
Luis Eduardo Oliveira Lizardo <lizardo.luis@gmail.com>
2017-10-29 23:29:54 +0100

    Change ast_topologicalrelationship to ast_spatialrelationship

--- resources/com/omtg2sql/sql/postgis/validate_topological_relationship_template.sql
+++ resources/com/omtg2sql/sql/postgis/validate_topological_relationship_template.sql
@@ -2,5 +2,5 @@
 CREATE TRIGGER <VAL_TOP_REL_NAME>_<SPATIAL_RELATION_MASK>_insert_update_trigger
   AFTER INSERT OR UPDATE ON <A_TABLE_NAME>
   FOR EACH STATEMENT
-  EXECUTE PROCEDURE ast_topologicalrelationship('<A_TABLE_NAME>', 'geom', '<B_TABLE_NAME>', 'geom', '<SPATIAL_RELATION>');
+  EXECUTE PROCEDURE ast_spatialrelationship('<A_TABLE_NAME>', 'geom', '<B_TABLE_NAME>', 'geom', '<SPATIAL_RELATION>');
 
